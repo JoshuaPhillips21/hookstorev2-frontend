3bf71aa04e46c6c2bdcd462f69b1520515c6cde6
JoshuaPhillips21 <joshua.t.phillips21@gmail.com>
2022-03-08 23:37:14 -0500

    test

---
+++ src/components/pages/navigation/navBar.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { A } from 'hookrouter';
+import Cookies from 'js-cookie';
+
+
+export default function NavBar(props) {
+    const [ loggedIn, setLoggedIn ] = useState(false)
+
+    const buttonType = () => {
+        if(Cookies.get('username')) {
+            console.log('found', Cookies.get())
+            setLoggedIn(false);
+            props.logout();
+        } else if(loggedIn && Cookies.get('username')){
+            console.log('not found', Cookies.get('username'))
+            props.login();
+        }
+    }
+
+    useEffect(() => {
+        if(Cookies.get('username')) {
+            setLoggedIn(true);
+        }
+    })
+
+    return (
+    <div className="navigation-container">
+        <div className="nav-link-wrapper">
+            <div className='nav-link'>
+                <A className="link" href='/'>
+                    Home
+                </A>
+            </div>
+            <div className='nav-link'>
+                <A className="link" href='/add'>
+                    Add-Book
+                </A>
+            </div>
+            <div className='nav-link'>
+                <A className="link" href='/signup'>
+                    {loggedIn ? '' : 'Sign Up'}
+                </A>
+            </div>
+            <div className='nav-link'>
+                <A className="link" href='/login' onClick={buttonType} >
+                    {loggedIn ? 'Logout' : 'Login'}
+                </A>
+            </div>
+        </div>
+    </div>
+    )
+}
